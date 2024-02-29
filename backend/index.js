76df34b0574722edf20dfcfa7923e693030bd702
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-02-29 13:43:47 +0530

    Added backend auth routes for user

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const {apiRouter} = require('./routes/index')
 const {userRouter} = require('./routes/user')
 const JWT_SECRET = require('./config')
 const cors = require('cors')
+const PORT = 3000
 
 const app = express()
 
@@ -11,6 +12,10 @@ app.use(express.json())
 app.use('/api/v1',apiRouter)
 app.use('api/v1/user',userRouter)
 
-app.listen(3000)
+
+
+app.listen(PORT,()=>{
+    console.log("Port is running on the port:"+PORT)
+})
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,83 @@
 const userRouter = require('express').Router()
+const {User} = require('../db')
+const z = require('zod')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../config')
 
-userRouter.get('/',(req,res)=>{
-    res.json({
-        msg : 'Hello user router'
+const UserValid = z.object({
+    username : z.string(),
+    email : z.email(),
+    firstName : z.string(),
+    lastName: z.string(),
+    password: z.string().min(6)
+})
+
+userRouter.post('/signup',async (req,res)=>{
+    const username = req.body.username;
+    const email = req.body.email;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const password = req.body.password;
+
+    const obj = {username,email,firstName,lastName,password}
+
+    const response = UserValid.safeParse(obj)
+
+    const foundUser = await User.findOne({
+        $or : [
+            {username},
+            {email}
+        ]
     })
+
+    if( foundUser || !response.success){
+        return res.status(411).json({
+            msg : "Email/Username already taken or incorrect input"
+        })
+    }
+
+    const userCreated = await User.create(obj);
+
+    const userId = userCreated._id;
+
+    const token = jwt.sign({userId},JWT_SECRET)
+
+    res.status(200).json({
+        message : "User created successfully",
+        token
+    })
+    
+})
+
+
+userRouter.post('/signin',async (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    //hash the password!!
+
+    const foundUser = await User.findOne({
+        $or : [
+            {username,password},
+            {email,password}
+        ]
+    })
+
+    if(!foundUser){
+        return res.status(411).json({
+            message : "Error while logging in !!"
+        })
+    }
+
+    const userId = foundUser._id;
+
+    const token = jwt.sign({userId},JWT_SECRET)
+
+    res.status(200).json({
+        token
+    })
+
 })
 
 module.exports = {
