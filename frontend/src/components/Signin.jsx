9cfd3f8005ebca31e694862f1eaf1374aaed2cbc
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-03-07 16:59:06 +0530

    signin page added

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
+import axios from 'axios'
+import { notify } from "../utils/notify";
+
+const url = 'http://localhost:3000/api/v1/user/signin'
 
 export const Signin = ()=>{
     const navigate = useNavigate();
+
+    const [user,setUser] = useState("")
+    const [pass,setPass] = useState("")
+
+    const signin = async ()=>{
+        setUser("")
+        setPass("")
+        try{
+            const response = await axios.post(url,{
+                username : user,
+                password: pass
+            })
+
+            localStorage.setItem('token',response.data.token)
+            notify("User logged in successfully!!",'s')
+            navigate('/dashboard')
+        }
+        catch(e){
+            console.log(e)
+            notify("Error while logging in !!",'d')
+        }
+    }
 
         return <div className="py-2 flex justify-center bg-gray-400 h-screen w-full">
             <div className="w-fit m-auto h-fit inline-block px-10 py-10 rounded-xl bg-white">
@@ -9,12 +36,12 @@ export const Signin = ()=>{
             <p className="text-center mt-2 text-gray-400">Enter your information to login</p>
     
             <h1 className="font-bold text-left mt-4">Username or Email address</h1>
-            <input type="text" className="w-full rounded-l  h-8 border-2 border-gray-10 mt-3 p-2" />
+            <input  onChange={(e)=>setUser(e.target.value)} value={user}  type="text" className="w-full rounded-l  h-8 border-2 border-gray-10 mt-3 p-2" />
     
             <h1 className="font-bold text-left mt-4">Password</h1>
-            <input type="password" className="w-full rounded-l  h-8 border-2 border-gray-10 mt-3 p-2" />
+            <input onChange={(e)=>setPass(e.target.value)} value={pass} type="password" className="w-full rounded-l  h-8 border-2 border-gray-10 mt-3 p-2" />
     
-            <button className="w-full mt-6 rounded-xl text-white font-normal h-12 border-2 bg-black">Sign In</button>
+            <button onClick={signin} className="w-full mt-6 rounded-xl text-white font-normal h-12 border-2 bg-black">Sign In</button>
             <div className="flex justify-center">
             <h1 className="inline-block mt-3">Don't have an account? </h1>
             <h1 className="inline-block ml-1 mt-3 cursor-pointer underline text-blue-500" onClick={()=>navigate('/signup')}> Register </h1>
